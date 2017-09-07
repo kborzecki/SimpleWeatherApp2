34508382bda1877526b03db54d3da6557865852d
Lagger96 <franek616@gmail.com>
2017-09-07 23:48:29 +0200

    app init && add fetch_weather action

---
+++ src/components/app.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import SearchBar from './searchbar.js'
+
+export default class App extends Component {
+  render() {
+    return (
+      <SearchBar />
+    )
+  }
+}
