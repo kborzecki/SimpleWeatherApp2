9910f6dbd53a2e8b2fcebafe431a68bb1385f7a1
Lagger96 <franek616@gmail.com>
2017-09-09 00:20:21 +0200

    change api request url

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 export const FETCH_WEATHER = 'FETCH_WEATHER'
 
-const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${config.API_KEY}`
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/weather?appid=${config.API_KEY}`
 
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},pl`
